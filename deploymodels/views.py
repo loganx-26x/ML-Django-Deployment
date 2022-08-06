e78bf3797a46764720da14df94ba7395f2ee9c74
Logan <lgnx@gmail.com>
2022-08-06 21:22:55 +0530

    Added Linear Regression:

--- deploymodels/views.py
+++ deploymodels/views.py
@@ -7,15 +7,28 @@ def home(request):
 
 
 def linearregression(request):
-    return render(request, 'deploymodels/linearregression.html')
+    modelresult = None
+    model_values = None
+    house_area = None
+    house_bedrooms = None
+    if request.GET.get('area'):
+        house_area = request.GET.get("area")
+        house_bedrooms = request.GET.get("bedrooms")
+        model = joblib.load("finalized_model.sav")
+        model_values = [house_area, house_bedrooms]
+
+        modelresult = str(int(model.predict([[int(house_area), int(house_bedrooms)]])))
+
+    return render(request, 'deploymodels/linearregression.html', {"modelresult": modelresult, "house_area": house_area,
+                                                                  "house_bedrooms": house_bedrooms})
 
 
 def result(request):
-
     model = joblib.load("finalized_model.sav")
 
-    model_values = [request.GET['price'], request.GET["area"]]
+    price = request.GET['price']
+    area = request.GET["area"]
 
-    modelresult=str(int(model.predict([[int(model_values[0]), int(model_values[1])]])))
+    modelresult = str(int(model.predict([[int(price), int(model_values[1])]])))
 
-    return render(request, "deploymodels/result.html", {"modelresult": modelresult, "prediction":model_values})
+    return render(request, "deploymodels/result.html", {"modelresult": modelresult, "prediction": model_values})
